cc8f9d6a34cf78e7499ab3e2ab7546ac35a41fc3
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-06-29 12:33:21 +0200

    job done

--- solution.js
+++ solution.js
@@ -1,1 +1,47 @@
+// task #1
 
+const str = "This is a boring string";
+
+console.log(str);
+
+// task #2
+console.log(str[1]);
+console.log(str[5]);
+
+//task #3
+const firstName = "Denzel Washington";
+
+//task #4
+
+const age = 53;
+
+//task #5
+
+const year = 2023;
+
+//task #6
+
+console.log(firstName + " is " + age + " in " + year);
+
+//task #7
+const isMarried = true;
+
+//task #8
+
+console.log(firstName + " is married: " + isMarried);
+
+//task #9
+
+const programmingLanguage = "Java Script";
+
+const isFun = true;
+
+console.log(programmingLanguage + " is fun: " + isFun);
+
+//task #10
+
+console.log(programmingLanguage[0], programmingLanguage[5]);
+
+//task #11
+
+console.log(programmingLanguage.length);
